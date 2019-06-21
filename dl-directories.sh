fc02ebf6dda07964c4e5f4ac2b5ee97100a80f9e
dm4uz3 <dm4uz3@gmail.com>
2019-06-21 13:00:41 +0100

    hashbang slash bin slash bash

--- dl-directories.sh
+++ dl-directories.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 NSLASH="$(echo "$1" | perl -pe 's|.*://[^/]+(.*?)/?$|\1|' | grep -o / | wc -l)"
 NCUT=$((NSLASH > 0 ? NSLASH-1 : 0))
 wget -e robots=off --no-check-certificate -r -c -nH --user-agent=Mozilla/5.0 --cut-dirs=$NCUT --no-parent --reject="index.html*" "$1"
